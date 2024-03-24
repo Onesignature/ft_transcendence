87f8436feba863ac27849f90abb49c40ca8ceb81
Bilal Saeed <bilalsaeedahmed1@gmail.com>
2024-03-25 03:55:16 +0400

    All elements being drawn on screen

--- main.js
+++ main.js
@@ -29,18 +29,60 @@ const playerOne = new Element({
     gravity: 2,
 });
 
+const playerTwo = new Element({
+    x: 625,
+    y: 200,
+    width: 15,
+    height: 80,
+    color: "#fff",
+    gravity: 2,
+});
+
+const ball = new Element({
+    x: 620/2,
+    y: 400/2,
+    width: 15,
+    height: 15,
+    color: "#20C20E",
+    gravity: 1,
+    speed: 1,
+});
+
 //second paddle
 
 //ball
-
-//player one score text
-
-//player two score text
 
 //draw elements
 function drawElement(element){
     context.fillStyle = element.color;
     context.fillRect(element.x, element.y, element.width, element.height);
 }
-drawElement(playerOne);
-//detect collision
+
+function displayScoreOne()
+{
+    context.font = "18px Arial"
+    context.fillStyle = '#fff'
+    context.fillText(scoreOne, canvas.width/2 - 60, 30)
+}
+
+function displayScoreTwo()
+{
+    context.font = "18px Arial"
+    context.fillStyle = '#fff'
+    context.fillText(scoreOne, canvas.width/2 + 60, 30)
+}
+
+function drawElements()
+{
+    context.clearRect(0,0,canvas.width, canvas.height);
+    drawElement(playerOne);
+    drawElement(playerTwo);
+    drawElement(ball);
+}
+
+function loop(){
+    drawElements();
+    window.requestAnimationFrame(loop)
+}
+
+loop();
