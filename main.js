a1f4543f2f24c4d26db0561696380acf6c79e282
Bilal Saeed <bilalsaeedahmed1@gmail.com>
2024-05-02 21:06:41 +0400

    Paddle movements working

--- main.js
+++ main.js
@@ -1,29 +1,154 @@
-document.body.style.backgroundColor = "black";
-
 const canvas = document.getElementById("pongGame");
 const context = canvas.getContext("2d");
 canvas.width = 650;
 canvas.height = 400;
 
-// Calculate the center position
-const centerX = (window.innerWidth - canvas.width) / 2;
-const centerY = (window.innerHeight - canvas.height) / 2;
-
-// Set the canvas position
-canvas.style.position = "absolute";
-canvas.style.left = centerX + "px";
-canvas.style.top = centerY + "px";
-
-context.fillStyle = "black";
-context.fillRect(0, 0, canvas.width, canvas.height);
-
-// Set the outline color
-context.strokeStyle = "yellow";
-context.lineWidth = 5; // Adjust the line width as needed
-
-// Draw a rectangle with an outline
-const outlineWidth = 10; // Width of the outline
-context.strokeRect(outlineWidth, outlineWidth, canvas.width - (2 * outlineWidth), canvas.height - (2 * outlineWidth));
-
 let scoreOne = 0;
 let scoreTwo = 0;
+
+//key movement
+window.addEventListener("keypress", doKeyDown, false);
+
+function doKeyDown(e)
+{
+    const key = e.key;
+    if (key == "w" && playerOne.y-playerOne.gravity > 0)
+        playerOne.y -= playerOne.gravity *4;
+    else if (key == "s" && playerOne.y+playerOne.gravity < canvas.height)
+        playerOne.y += playerOne.gravity * 4;
+
+    if (key == "i" && playerTwo.y-playerTwo.gravity > 0)
+        playerTwo.y -= playerTwo.gravity *4;
+    else if (key == "k" && playerTwo.y+playerTwo.gravity < canvas.height)
+        playerTwo.y += playerTwo.gravity * 4;
+}
+
+class Element{
+    constructor(options){
+        this.x = options.x;
+        this.y = options.y;
+        this.width = options.width;
+        this.height = options.height;
+        this.color = options.color;
+        this.speed = options.speed || 2;
+        this.gravity = options.gravity;
+    }
+}
+
+
+//paddle1
+const playerOne = new Element
+(
+    {
+        x: 10,
+        y: 200,
+        width: 15,
+        height: 80,
+        color: '#ffd335',
+        gravity: 2,
+    }
+);
+
+//paddle2
+const playerTwo = new Element
+(
+    {
+        x: 625,
+        y: 200,
+        width: 15,
+        height: 80,
+        color: '#ffd335',
+        gravity: 2,
+    }
+);
+
+//ball
+const ball = new Element
+(
+    {
+        x: 650 / 2,
+        y: 400 / 2,
+        width: 15,
+        height: 15,
+        color: '#fff',
+        speed: 1,
+        gravity: 1,
+    }
+);
+
+//player one text
+function displayScoreOne()
+{
+    context.font = "18px Arial"
+    context.fillStyle = "#ffd335"
+    context.fillText(scoreOne, canvas.width / 2 - 60, 30);
+}
+
+//player two text
+function displayScoreTwo()
+{
+    context.font = "18px Arial"
+    context.fillStyle = "#ffd335"
+    context.fillText(scoreOne, canvas.width / 2 + 60, 30);
+}
+
+//draw elements
+function drawElement(element)
+{
+    context.fillStyle = element.color;
+    context.fillRect(element.x, element.y, element.width, element.height);
+}
+
+// drawElement(playerOne);
+
+//make Ball bounce
+function ballBounce()
+{
+    if (ball.y + ball.gravity <= 0 || ball.y + ball.gravity >= canvas.height)
+    {
+        ball.gravity = ball.gravity * -1;
+        ball.y += ball.gravity;
+        ball.x += ball.speed;
+    }
+    else
+    {
+        ball.y += ball.gravity;
+        ball.x += ball.speed;
+    }
+ballWallCollision();
+}
+
+//detect collision
+function ballWallCollision()
+{
+    if (ball.x + ball.speed <= 0 || ball.x + ball.speed + ball.width >= canvas.width)
+    {
+        ball.y += ball.gravity;
+        ball.speed  = ball.speed * -1;
+        ball.x += ball.speed;
+    }
+    else
+    {
+        ball.y += ball.gravity;
+        ball.x += ball.speed;
+    }
+drawElements();
+}
+
+//drawelements
+function drawElements()
+{
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawElement(playerOne);
+    drawElement(playerTwo);
+    drawElement(ball);
+    displayScoreOne();
+    displayScoreTwo();
+}
+
+function loop()
+{
+    ballBounce();
+    window.requestAnimationFrame(loop)
+}
+loop();
