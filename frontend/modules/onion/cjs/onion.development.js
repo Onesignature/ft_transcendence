3cc50b366d69bc9d95fb80ef25053387285f3e33
Faraz <faraz7704@gmail.com>
2024-05-31 16:48:32 +0400

    added initial onion framework setup files

--- frontend/modules/onion/cjs/onion.development.js
+++ frontend/modules/onion/cjs/onion.development.js
@@ -9,6 +9,26 @@
         this.context = context;
     }
 
+    Component.prototype.onEnable()
+    {}
+
+    Component.prototype.onUpdate(prevProps, prevState)
+    {}
+
+    Component.prototype.onDisable()
+    {}
+
+    Component.prototype.render()
+    {}
+
+    Component.prototype.shouldComponentUpdate()
+    {
+        return true;
+    }
+
+    Component.prototype.shouldComponentUpdate()
+    {}
+
     exports.Component = Component;
 
 })();
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,3 @@
-import Onion from 'onion';
+import onion from "../modules/onion";
 
 const root = document.getElementById('root');
-
-Onion.Component();
-
-root.appendChild('<h1>Here</h1>');
